186f82bed8cbea18df34eb094043602d7106744d
Gautier <gautier.debrabander@decathlon.com>
2019-01-13 12:34:13 +0100

    feat(mock) add some mock

--- scripts/sender.js
+++ scripts/sender.js
@@ -1,64 +1,32 @@
-MOCKSERVICE= {
-  url_profil_picture:"https://www.lyonmag.com/medias/images/stephane-bern45.jpg",
-  profil_name:"Stephane",
-  description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-  uev:150,
-  time:"1h30",
-  square:"Lille"}
-
-MOCKCONVERSATION = {
-  url_profil_picture:"https://www.lyonmag.com/medias/images/stephane-bern45.jpg",
-  profil_name:"Stephane",
-  last_message:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-  time:"1h30",
-}
-
-MOCKCOMMENTARY = {profil_name:"Stephane",text:"Genial experience avec moi meme !"}
-
-MOCKCOMMUNITY_SMALL = {
-  name:"Jardinage et potager",
-  description:"Bonjour, ici se retrouvent tous les fans de potager et de jardinage, conseils,services zefzefzefze zefzefzef zfefethyre",
-  members:256,
-  partages:178,
-  square:"Lille"
-}
-const MOCKCOMMUNITY= {
-  name:"Jardinage et potager",
-  owner_name:"Sebastien",
-  members:256,
-  square:"Lille",
-  messages:[MOCKSERVICE,MOCKSERVICE,MOCKSERVICE]
-}
-
-
+communitiesSmallToLoad = [JARDIN_ET_POTAGER_SMALL,BRICOLAGE_SMALL]
 function loadCommunities(){
-  let communities = [MOCKCOMMUNITY_SMALL,MOCKCOMMUNITY_SMALL,MOCKCOMMUNITY_SMALL]
+  let communities = communitiesSmallToLoad;
   return communities;
 }
 
 function loadCommunity(){
-  let community = Object.assign({},MOCKCOMMUNITY);
+  let community = Object.assign({},JARDIN_ET_POTAGER_SMALL);
   let messagesHTML = ""
   for (var i=0;i<community.messages.length;i++){
-    messagesHTML += service(MOCKCOMMUNITY.messages[i])
+    messagesHTML += service(JARDIN_ET_POTAGER_SMALL.messages[i])
   }
   community.messages = messagesHTML;
   return community;
 }
 function loadCommentaries(idProfil){
-  let commentaries = [MOCKCOMMENTARY,MOCKCOMMENTARY,MOCKCOMMENTARY]
+  let commentaries = [COMMENTARY_VALERIE_1,COMMENTARY_VALERIE_2,COMMENTARY_VALERIE_3]
   return commentaries;
 }
 function loadServices(){
-  let services = [MOCKSERVICE,MOCKSERVICE,MOCKSERVICE]
+  let services = [SERVICE_BRICOLAGE_1,SERVICE_BRICOLAGE_2]
   return services;
 }
 
 function loadService(){
-  let service = MOCKSERVICE
+  let service = SERVICE_JARDIN_1
   return service;
 }
 function loadConversations(){
-  let conversations = [MOCKCONVERSATION,MOCKCONVERSATION,MOCKCONVERSATION]
+  let conversations = [CONVERSATION_VALERIE_1]
   return conversations;
 }
