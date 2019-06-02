ce05f26b46fa9d5f7b5cdc7b4e822b6c704506a7
Gautier <gautier.debrabander@decathlon.com>
2019-06-02 10:36:43 +0200

    save

--- scripts/State.js
+++ scripts/State.js
@@ -5,14 +5,31 @@ function init(){
   socket = io('http://localhost:8080');
   logged = false;
   loggedsElement = document.getElementsByClassName("logged");
+  unloggedsElement = document.getElementsByClassName("unlogged");
   for (var i=0;i<loggedsElement.length;i++){
-    loggedsElement[0].style.display="none";
+    loggedsElement[i].style.display="none";
+  }
+  for (var i=0;i<unloggedsElement.length;i++){
+    unloggedsElement[0].style.display="";
   }
 }
 
 function login(){
   logged = true;
   for (var i=0;i<loggedsElement.length;i++){
-    loggedsElement[0].style.display="";
+    loggedsElement[i].style.display="";
+  }
+  for (var i=0;i<unloggedsElement.length;i++){
+    unloggedsElement[i].style.display="none";
+  }
+}
+
+function logout(){
+  logged = false;
+  for (var i=0;i<loggedsElement.length;i++){
+    loggedsElement[i].style.display="none";
+  }
+  for (var i=0;i<unloggedsElement.length;i++){
+    unloggedsElement[i].style.display="";
   }
 }
