234d74454fd27a67bbe81278f6480d02d023acc1
Gautier <gautier.debrabander@decathlon.com>
2019-06-04 10:16:57 +0200

    upd

--- scripts/communities.js
+++ scripts/communities.js
@@ -11,8 +11,67 @@ function initCommunities(){
       city:"Lille"
     }
   )
-  communitiesListCommunities.innerHTML+= small_community({name:"Jardin et Potager", description:"Bonjour, ici se trouvent tout les fans de potager et de jardinage, conseils, services. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium reprehenderit laudantium possimus culpa sapiente voluptas consequatur suscipit molestias voluptatibus obcaecati.",members_number:30,share_number:50,city:"Lille"})
-  communitiesListCommunities.innerHTML+= small_community({name:"Jardin et Potager", description:"Bonjour, ici se trouvent tout les fans de potager et de jardinage, conseils, services. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium reprehenderit laudantium possimus culpa sapiente voluptas consequatur suscipit molestias voluptatibus obcaecati.",members_number:30,share_number:50,city:"Lille"})
-  communitiesListCommunities.innerHTML+= small_community({name:"Jardin et Potager", description:"Bonjour, ici se trouvent tout les fans de potager et de jardinage, conseils, services. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium reprehenderit laudantium possimus culpa sapiente voluptas consequatur suscipit molestias voluptatibus obcaecati.",members_number:30,share_number:50,city:"Lille"})
-  communitiesListCommunities.innerHTML+= small_community({name:"Jardin et Potager", description:"Bonjour, ici se trouvent tout les fans de potager et de jardinage, conseils, services. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium reprehenderit laudantium possimus culpa sapiente voluptas consequatur suscipit molestias voluptatibus obcaecati.",members_number:30,share_number:50,city:"Lille"})
+  communitiesListCommunities.innerHTML+= small_community(
+    {
+      name:"Jardin et Potager",
+      description:"Bonjour, ici se trouvent tout les fans de potager et de jardinage, conseils, services. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium reprehenderit laudantium possimus culpa sapiente voluptas consequatur suscipit molestias voluptatibus obcaecati.",
+      members_number:30,
+      share_number:50,
+      city:"Lille"
+    }
+  )
+  communitiesListCommunities.innerHTML+= small_community(
+    {
+      name:"Jardin et Potager",
+      description:"Bonjour, ici se trouvent tout les fans de potager et de jardinage, conseils, services. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium reprehenderit laudantium possimus culpa sapiente voluptas consequatur suscipit molestias voluptatibus obcaecati.",
+      members_number:30,
+      share_number:50,
+      city:"Lille"
+    }
+  )
+  communitiesListCommunities.innerHTML+= small_community(
+    {
+      name:"Jardin et Potager",
+      description:"Bonjour, ici se trouvent tout les fans de potager et de jardinage, conseils, services. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium reprehenderit laudantium possimus culpa sapiente voluptas consequatur suscipit molestias voluptatibus obcaecati.",
+      members_number:30,
+      share_number:50,
+      city:"Lille"
+    }
+  )
+  communitiesListCommunities.innerHTML+= small_community(
+    {
+      name:"Jardin et Potager",
+      description:"Bonjour, ici se trouvent tout les fans de potager et de jardinage, conseils, services. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium reprehenderit laudantium possimus culpa sapiente voluptas consequatur suscipit molestias voluptatibus obcaecati.",
+      members_number:30,
+      share_number:50,
+      city:"Lille"
+    }
+  )
+  communitiesListCommunities.innerHTML+= small_community(
+    {
+      name:"Jardin et Potager",
+      description:"Bonjour, ici se trouvent tout les fans de potager et de jardinage, conseils, services. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium reprehenderit laudantium possimus culpa sapiente voluptas consequatur suscipit molestias voluptatibus obcaecati.",
+      members_number:30,
+      share_number:50,
+      city:"Lille"
+    }
+  )
+  communitiesListCommunities.innerHTML+= small_community(
+    {
+      name:"Jardin et Potager",
+      description:"Bonjour, ici se trouvent tout les fans de potager et de jardinage, conseils, services. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium reprehenderit laudantium possimus culpa sapiente voluptas consequatur suscipit molestias voluptatibus obcaecati.",
+      members_number:30,
+      share_number:50,
+      city:"Lille"
+    }
+  )
+  communitiesListCommunities.innerHTML+= small_community(
+    {
+      name:"Jardin et Potager",
+      description:"Bonjour, ici se trouvent tout les fans de potager et de jardinage, conseils, services. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium reprehenderit laudantium possimus culpa sapiente voluptas consequatur suscipit molestias voluptatibus obcaecati.",
+      members_number:30,
+      share_number:50,
+      city:"Lille"
+    }
+  )
 }
